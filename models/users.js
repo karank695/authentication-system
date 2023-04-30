a0abbf1a61d5ddbf5aee8b1d59b9d28b28ebfb9c
karank695 <116067550+karank695@users.noreply.github.com>
2023-04-30 21:43:53 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String,required:true },
-    phone: { type: String,required:true },
+    phone: { type: String },
     password: { type: String,required:true },
     dob: { type: Date},
-    gender:{type:String,required:true}
+    gender:{type:String}
 });
 
 const User = mongoose.model('User', userSchema);
